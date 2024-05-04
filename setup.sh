40d279432fbbc50ddf847f91dae7897fc8c4777f
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-05-04 13:32:04 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -114,7 +114,7 @@ echo -e "[ ${green}INFO${NC} ] Alright good ... installation file is ready"
 sleep 2
 echo -ne "[ ${green}INFO${NC} ] Check permission : "
 
-PERMISSION
+#PERMISSION
 if [ -f /home/needupdate ]; then
 red "Your script need to update first !"
 exit 0
