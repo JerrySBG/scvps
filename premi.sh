12f52b32e67d63874852cefbe254c589117f53f6
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-07-27 15:17:03 -0500

    1.0

--- premi.sh
+++ premi.sh
@@ -815,23 +815,19 @@ systemctl enable ws
 systemctl start ws
 systemctl restart ws
 wget -O /usr/bin/ws2 "${REPO}limit/ws2.py" >/dev/null 2>&1
-wget -O /usr/bin/tun2.conf "${REPO}limit/tun2.conf" >/dev/null 2>&1
     wget -O /etc/systemd/system/ws2.service "${REPO}limit/ws2.service" >/dev/null 2>&1
     chmod +x /etc/systemd/system/ws2.service
     chmod +x /usr/bin/ws2
-    chmod 644 /usr/bin/tun2.conf
-systemctl disable ws2
+    systemctl disable ws2
 systemctl stop ws2
 systemctl enable ws2
 systemctl start ws2
 systemctl restart ws2
 wget -O /usr/bin/ws3 "${REPO}limit/ws3.py" >/dev/null 2>&1
-wget -O /usr/bin/tun3.conf "${REPO}limit/tun3.conf" >/dev/null 2>&1
     wget -O /etc/systemd/system/ws3.service "${REPO}limit/ws3.service" >/dev/null 2>&1
     chmod +x /etc/systemd/system/ws3.service
     chmod +x /usr/bin/ws3
-    chmod 644 /usr/bin/tun3.conf
-systemctl disable ws3
+    systemctl disable ws3
 systemctl stop ws3
 systemctl enable ws3
 systemctl start ws3
