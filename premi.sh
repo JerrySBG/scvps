33958b89725d3e1d48a45e135289db70835c5c45
JerrySBG <165622937+JerrySBG@users.noreply.github.com>
2024-08-22 23:15:45 -0500

    1.0

--- premi.sh
+++ premi.sh
@@ -342,6 +342,41 @@ wget ${REPO}cf.sh && chmod +x cf.sh && ./cf.sh
 rm -f /root/cf.sh
 clear
     fi
+
+#THEME RED
+cat <<EOF>> /etc/byjerrysbg/theme/red
+BG : \E[40;1;41m
+TEXT : \033[0;31m
+EOF
+#THEME BLUE
+cat <<EOF>> /etc/byjerrysbg/theme/blue
+BG : \E[40;1;44m
+TEXT : \033[0;34m
+EOF
+#THEME GREEN
+cat <<EOF>> /etc/byjerrysbg/theme/green
+BG : \E[40;1;42m
+TEXT : \033[0;32m
+EOF
+#THEME YELLOW
+cat <<EOF>> /etc/byjerrysbg/theme/yellow
+BG : \E[40;1;43m
+TEXT : \033[0;33m
+EOF
+#THEME MAGENTA
+cat <<EOF>> /etc/byjerrysbg/theme/magenta
+BG : \E[40;1;43m
+TEXT : \033[0;33m
+EOF
+#THEME CYAN
+cat <<EOF>> /etc/byjerrysbg/theme/cyan
+BG : \E[40;1;46m
+TEXT : \033[0;36m
+EOF
+#THEME CONFIG
+cat <<EOF>> /etc/byjerrysbg/theme/color.conf
+blue
+EOF
 }
 
 clear
